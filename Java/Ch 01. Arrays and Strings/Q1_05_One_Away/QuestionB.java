edc185585ffcfffe9e41e973c5c1612a123f1e13
Michael Aristarkhov <aristarkhov.m@gmail.com>
2015-07-12 20:33:16 +0200

    changed condition in QuestionB to pass "pale"-"pkle" test

--- Java/Ch 01. Arrays and Strings/Q1_05_One_Away/QuestionB.java
+++ Java/Ch 01. Arrays and Strings/Q1_05_One_Away/QuestionB.java
@@ -1,7 +1,7 @@
 package Q1_05_One_Away;
 
 public class QuestionB {	
-	public static boolean oneEditAway(String first, String second) {
+public static boolean oneEditAway(String first, String second) {
 		/* Length checks. */
 		if (Math.abs(first.length() - second.length()) > 1) {
 			return false;
@@ -10,7 +10,7 @@ public class QuestionB {
 		/* Get shorter and longer string.*/
 		String s1 = first.length() < second.length() ? first : second;
 		String s2 = first.length() < second.length() ? second : first;
-		
+
 		int index1 = 0;
 		int index2 = 0;
 		boolean foundDifference = false;
@@ -18,14 +18,15 @@ public class QuestionB {
 			if (s1.charAt(index1) != s2.charAt(index2)) {
 				if (foundDifference) return false;
 				foundDifference = true;
-				
+
 				if (s1.length() < s2.length()) {
 					index2++;
+					continue;
 				}
-			} else {
-				index1++;
-				index2++;
 			}
+			index1++;
+			index2++;
+
 		}
 		return true;
 	}
@@ -35,8 +36,13 @@ public class QuestionB {
 	public static void main(String[] args) {
 		String a = "pse";
 		String b = "pale";
-		boolean isOneEdit = oneEditAway(a, b);
-		System.out.println(a + ", " + b + ": " + isOneEdit);
+		boolean isOneEdit1 = oneEditAway(a, b);
+		System.out.println(a + ", " + b + ": " + isOneEdit1);
+
+		String c = "pale";
+		String d = "pkle";
+		boolean isOneEdit2 = oneEditAway(c, d);
+		System.out.println(c + ", " + d + ": " + isOneEdit2);
 	}
 
 }
