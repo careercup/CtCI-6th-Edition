93ccc0b94d42b1ccc120fd8cf8cc76e24e91d7a0
Kunal Vyas <kv.kunalvyas@gmail.com>
2015-09-15 17:17:41 -0400

    1.1

---
+++ Python/Chapter 1/1_1.py
@@ -0,0 +1,26 @@
+# O(N)
+
+import unittest
+
+def unique(string):
+  count = 0
+  hash_table = {}
+  for character in string:
+    count += 1
+    if character not in hash_table.values():
+      hash_table[count] = character
+  if(len(hash_table) == count):
+    return True
+  else:
+    return False
+
+class Test(unittest.TestCase):
+  data = [('abcd', True), ('23fdsf2', False), ('swe1234fad', True), ('hb 627 jh =j ()', False), ('', True)]
+
+  def test_unique(self):
+    for test_string in self.data:
+      actual = unique(test_string)
+      self.assertTrue(actual)
+
+if __name__ == "__main__":
+  unittest.main()
