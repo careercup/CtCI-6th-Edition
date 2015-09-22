425638b86cbe42638f863652e53b1430ad4bc30a
Gayle McDowell <gayle@careercup.com>
2015-09-22 00:38:12 -0700

    4.10 Updated first solution to only check if pre-order traversal is equal

--- Java/Ch 04. Trees and Graphs/Q4_10_Check_Subtree/QuestionA.java
+++ Java/Ch 04. Trees and Graphs/Q4_10_Check_Subtree/QuestionA.java
@@ -4,40 +4,31 @@ import CtCILibrary.AssortedMethods;
 import CtCILibrary.TreeNode;
 
 public class QuestionA {
-	public static enum Order {
-		PRE, IN, POST
-	}
 	
 	public static boolean containsTree(TreeNode t1, TreeNode t2) {
-		return isTraversalSubstring(t1, t2, Order.PRE) && isTraversalSubstring(t1, t2, Order.IN);
-	}
-	
-	public static boolean isTraversalSubstring(TreeNode t1, TreeNode t2, Order order) {
 		StringBuilder string1 = new StringBuilder();
 		StringBuilder string2 = new StringBuilder();
 		
-		getOrderString(t1, string1, order);
-		getOrderString(t2, string2, order);
+		getOrderString(t1, string1);
+		getOrderString(t2, string2);
 		
 		return string1.indexOf(string2.toString()) != -1;
 	}
 	
-	public static void getOrderString(TreeNode node, StringBuilder sb, Order order) {
+	public static void getOrderString(TreeNode node, StringBuilder sb) {
 		if (node == null) {
-			sb.append("X"); // Add null indicator
+			sb.append("X");             // Add null indicator
 			return;
 		}
-		if (order == Order.PRE) sb.append(node.data); // Add root if preorder
-		getOrderString(node.left, sb, order); // Add left
-		if (order == Order.IN) sb.append(node.data); // Add root if inorder
-		getOrderString(node.right, sb, order); // Add right
-		if (order == Order.POST) sb.append(node.data); // Add root if postorder 
+		sb.append(node.data);           // Add root 
+		getOrderString(node.left, sb);  // Add left
+		getOrderString(node.right, sb); // Add right
 	}
 
 	public static void main(String[] args) {
 		// t2 is a subtree of t1
 		int[] array1 = {1, 2, 1, 3, 1, 1, 5};
-		int[] array2 = {1, 1, 1};
+		int[] array2 = {2, 3, 1};
 		
 		TreeNode t1 = AssortedMethods.createTreeFromArray(array1);
 		TreeNode t2 = AssortedMethods.createTreeFromArray(array2);
