1f73e38a8b047b46044dca2c25a89658936d1edd
Gayle McDowell <gayle@careercup.com>
2015-09-22 00:38:34 -0700

    5.03 minor comment edits

--- Java/Ch 05. Bit Manipulation/Q5_03_Flip_Bit_to_Win/QuestionD.java
+++ Java/Ch 05. Bit Manipulation/Q5_03_Flip_Bit_to_Win/QuestionD.java
@@ -3,15 +3,17 @@ package Q5_03_Flip_Bit_to_Win;
 public class QuestionD {
 
 	public static int flipBit(int a) {
-		if (~a == 0) return Integer.BYTES * 8; // If all 1s, this is already the longest sequence
+		/* If all 1s, this is already the longest sequence. */
+		if (~a == 0) return Integer.BYTES * 8;
 		
 		int currentLength = 0;
 		int previousLength = 0;
-		int maxLength = 1;
+		int maxLength = 1; // We can always have a sequence of at least one 1
 		while (a != 0) {
 			if ((a & 1) == 1) {
 				currentLength++;
 			} else if ((a & 1) == 0) {
+				/* Update to 0 (if next bit is 0) or currentLength (if next bit is 1). */
 				previousLength = (a & 2) == 0 ? 0 : currentLength;
 				currentLength = 0;
 			}
@@ -19,7 +21,7 @@ public class QuestionD {
 			a >>>= 1;
 		}
 		return maxLength;
-	}	 	
+	}
 	
 	public static void main(String[] args) {
 		int[][] cases = {{-1, 32}, {Integer.MAX_VALUE, 32}, {-10, 31}, {0, 1}, 
