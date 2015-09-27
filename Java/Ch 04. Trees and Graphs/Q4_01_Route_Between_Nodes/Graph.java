fd505680aae9a4b979e3f4b67c8e21f6cf864228
Kim, Henry <Henry_Kim@intuit.com>
2015-09-26 21:52:41 -0700

    Ch 04 Node.java : Check for correct array max when adding node.  It was checking for 30.

--- Java/Ch 04. Trees and Graphs/Q4_01_Route_Between_Nodes/Graph.java
+++ Java/Ch 04. Trees and Graphs/Q4_01_Route_Between_Nodes/Graph.java
@@ -1,15 +1,16 @@
 package Q4_01_Route_Between_Nodes;
 
 public class Graph {
+	public static int MAX_VERTICES = 6;
 	private Node vertices[];
 	public int count;
 	public Graph() {
-		vertices = new Node[6];
+		vertices = new Node[MAX_VERTICES];
 		count = 0;
     }
 	
     public void addNode(Node x) {
-		if (count < 30) {
+		if (count < vertices.length) {
 			vertices[count] = x;
 			count++;
 		} else {
