26244d23187fffb9d560bb5d7315b275224d79b7
Leeor Engel <leeorengel@gmail.com>
2015-10-10 15:34:11 -0400

    Solutions for Q4 & Q7, minor edit to namespace in Q2 test

---
+++ Clojure/test/chapter_1/chapter_1_q7_test.clj
@@ -0,0 +1,21 @@
+(ns chapter-1.chapter-1-q7-test
+  (:require [clojure.test :refer :all]
+            [chapter-1.chapter-1-q7 :refer :all]))
+
+(deftest rotate-matrix-test
+  (are [m] (= (nth (iterate rotate-matrix m) 4) m)
+           [[1]]
+           [[1 2] [3 4]]
+           [[1 2 3] [4 5 6] [7 8 9]]
+           [[1 2 3 4] [5 6 7 8] [9 10 11 12] [13 14 15 16]]
+           [[11 1 34 2 36] [44 56 6 78 87] [17 88 900 12 42] [23 81 564 35 78] [2 66 31 9 0]]))
+
+(deftest rotate-matrix-in-place-test
+  (are [m] (= (nth (iterate rotate-matrix-in-place m) 4) m)
+           [[1]]
+           [[1 2] [3 4]]
+           [[1 2 3] [4 5 6] [7 8 9]]
+           [[1 2 3 4] [5 6 7 8] [9 10 11 12] [13 14 15 16]]
+           [[11 1 34 2 36] [44 56 6 78 87] [17 88 900 12 42] [23 81 564 35 78] [2 66 31 9 0]]))
+
+(run-tests)
