ee44b7780086cc300925f5590e55c5fde6d05263
Leeor Engel <leeor@bibliocommons.com>
2015-10-30 22:38:08 -0400

    Chapter 3 Q1

--- Clojure/src/chapter_3/chapter_3_q1.clj
+++ Clojure/src/chapter_3/chapter_3_q1.clj
@@ -7,44 +7,44 @@
   (stack-push [this stack-num x])
   (stack-empty? [this stack-num]))
 
-(defn- peek-with-fn [stack-num stacks post-peek-fn]
+(defn- peek-with-fn [n stack-num stacks post-peek-fn]
   {:pre [(<= 1 stack-num 3)]}
   (let [tops (:tops stacks)
         arr (:arr stacks)
         tops-vec @tops
         to-pop-idx (get tops-vec (dec stack-num))]
-    (if (>= to-pop-idx 0)
+    (if (>= to-pop-idx (* n (dec stack-num)))
       (let [popped (aget arr to-pop-idx)]
         (when (some? post-peek-fn)
           (post-peek-fn tops to-pop-idx))
         popped)
       nil)))
 
-(defrecord ThreeStacks [arr ^Atom tops]
+(defrecord ThreeStacks [n arr ^Atom tops]
   Stacks
   (stack-pop [this stack-num]
-    (peek-with-fn stack-num this (fn [^Atom tops to-pop-idx]
-                                   (let [new-top (- to-pop-idx 3)]
+    (peek-with-fn n stack-num this (fn [^Atom tops to-pop-idx]
+                                   (let [new-top (dec to-pop-idx)]
                                      (swap! tops assoc (dec stack-num) new-top)))))
 
   (stack-peek [this stack-num]
-    (peek-with-fn stack-num this nil))
+    (peek-with-fn n stack-num this nil))
 
   (stack-push [this stack-num x]
-    (if (let [max-idx (- (dec (alength arr)) (- 3 stack-num))]
+    (if (let [max-idx (dec (* n stack-num))]
           (= max-idx (get @tops (dec stack-num))))
       (throw (IllegalStateException. "Stack full!"))
       (let [tops-vec @tops
             cur-top-idx (get tops-vec (dec stack-num))
-            to-push-idx (+ cur-top-idx 3)]
+            to-push-idx (inc cur-top-idx)]
         (aset arr to-push-idx x)
         (swap! tops assoc (dec stack-num) to-push-idx))))
 
   (stack-empty? [this stack-num]
     (if (<= 1 stack-num 3)
       (let [tops-vec @tops]
-        (neg? (get tops-vec (dec stack-num))))
+        (< (get tops-vec (dec stack-num)) (* n (dec stack-num))))
       (throw (IllegalArgumentException. "Invalid stack number!")))))
 
 (defn create-three-stacks [n]
-  (->ThreeStacks (make-array Object (* n 3)) (atom [-3 -2 -1])))
+  (->ThreeStacks n (make-array Object (* n 3)) (atom [(dec (* n 0)) (dec (* n 1)) (dec (* n 2))])))
