133c9fc64cbf90a13655e7f1dce158153e092e7d
Diwas Adhikary <diadical@diadic.net>
2015-11-06 19:09:09 +1000

    Added solution to ch3-1 and associated tests.

---
+++ javascript-es2015/src/chapter3/ch3-q1.js
@@ -0,0 +1,44 @@
+'use strict';
+
+export class TripleStack {
+  constructor() {
+    this._array = [];
+    this._lengths = [0, 0, 0];
+  }
+
+  _getLength(stack) {
+    return this._lengths[stack - 1];
+  }
+
+  push(stack, value) {
+    let idx = this._getLength(stack) * 3 + stack - 1;
+    this._array[idx] = value;
+    ++this._lengths[stack - 1];
+  }
+
+  pop(stack) {
+    let length = this._getLength(stack),
+      value;
+    if (length > 0) {
+      let idx = (length - 1) * 3 + stack - 1;
+      value = this._array[idx];
+      this._array[idx] = undefined;
+      --this._lengths[stack - 1];
+    }
+    return value;
+  }
+
+  peek(stack) {
+    let length = this._getLength(stack),
+      value;
+    if (length > 0) {
+      let idx = (length - 1) * 3 + stack - 1;
+      value = this._array[idx];
+    }
+    return value;
+  }
+
+  isEmpty(stack) {
+    return this._getLength(stack) === 0;
+  }
+}
