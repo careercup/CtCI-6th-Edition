cef0f1d9cc95007a474ac7792369ba4073cb617c
Diwas Adhikary <diadical@diadic.net>
2015-11-06 19:15:17 +1000

    Added JSDoc for ch3-1 solution.

--- javascript-es2015/src/chapter3/ch3-q1.js
+++ javascript-es2015/src/chapter3/ch3-q1.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/**
+ * TripleStack class holds 3 stacks in one array. This is done by interleaving
+ * the values from the 3 indexes, so the first items are at 0, 1 and 2 and
+ * subsequent items are every 3 places from those. This class takes advantage
+ * of the fact that JavaScript arrays are dynamic and doesn't hold the stacks
+ * to any size. It doesn't reduce the size of the underlying array when items
+ * are popped but that could easily be added.
+ *
+ * Time: push O(1), pop O(1), peek O(1)
+ * Additional space: push O(1), pop O(1), peek O(1)
+ */
 export class TripleStack {
   constructor() {
     this._array = [];
