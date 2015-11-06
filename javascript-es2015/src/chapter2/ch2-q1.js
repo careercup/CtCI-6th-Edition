fdb6fe0a1157464eb3dcb4e500ec2d5cf67c5085
Diwas Adhikary <diadical@diadic.net>
2015-11-06 12:28:13 +1000

    Updated JSDoc for ch2-1.

--- javascript-es2015/src/chapter2/ch2-q1.js
+++ javascript-es2015/src/chapter2/ch2-q1.js
@@ -1,7 +1,12 @@
+'use strict';
+
 /**
- * [removeDuplicatesSet description]
- * @param  {[type]} list [description]
- * @return {[type]}      [description]
+ * Iterate through list keeping a Set of all the values seen. If a seen value is
+ * seen again in the list then skip over it.
+ *
+ * N = |list|
+ * Time: O(N) -> Assuming Set is a HashSet structure with O(1) access times
+ * Additional space: O(N)
  */
 export function removeDuplicatesSet(list) {
   if (!list) {
