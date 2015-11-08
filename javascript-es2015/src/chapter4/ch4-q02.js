ce9c77a86e7f6d0348419443b46787eac5e56c2e
Diwas Adhikary <diadical@diadic.net>
2015-11-08 17:47:30 +1000

    Added JSDoc for ch4-2.

--- javascript-es2015/src/chapter4/ch4-q02.js
+++ javascript-es2015/src/chapter4/ch4-q02.js
@@ -13,6 +13,16 @@ function add(tree, values, start, end) {
   }
 }
 
+/**
+ * As the list is already sorted the best way to create a balanced tree is by
+ * adding the middle node (parent) then the children. The algorithm is basically
+ * involves adding the middle element of which split of the array so that the
+ * parent is added before the left and right children of each subtree.
+ *
+ * N = |values|
+ * Time: O(N lg N)
+ * Additional space: O(N)
+ */
 export function makeBalancedTree(values) {
   let tree = new Tree();
   if (values && values.length) {
