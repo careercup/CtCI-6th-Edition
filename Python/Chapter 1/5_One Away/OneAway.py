0b8871373fe348109b922505062368725676e8b6
Mertcan Mermerkaya <mertcan@mermerkaya.net>
2015-11-26 21:25:39 +0200

    Remaining Python solutions for Chapter 1

---
+++ Python/Chapter 1/5_One Away/OneAway.py
@@ -0,0 +1,55 @@
+# O(N)
+import unittest
+
+
+def one_away(s1, s2):
+    '''Check if a string can converted to another string with a single edit'''
+    if len(s1) == len(s2):
+        return one_edit_replace(s1, s2)
+    elif len(s1) + 1 == len(s2):
+        return one_edit_insert(s1, s2)
+    elif len(s1) - 1 == len(s2):
+        return one_edit_insert(s2, s1)
+    return False
+
+
+def one_edit_replace(s1, s2):
+    edited = False
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            if edited:
+                return False
+            edited = True
+    return True
+
+
+def one_edit_insert(s1, s2):
+    edited = False
+    i, j = 0, 0
+    while i < len(s1) and j < len(s2):
+        if s1[i] != s2[j]:
+            if edited:
+                return False
+            edited = True
+            i += 1
+        i += 1
+        j += 1
+    return True
+
+
+class Test(unittest.TestCase):
+    '''Test Cases'''
+    data = [
+        ('pale', 'ple', True),
+        ('pales', 'pale', True),
+        ('pale', 'bale', True),
+        ('pale', 'ble', False)
+    ]
+
+    def test_one_away(self):
+        for [test_s1, test_s2, expected] in self.data:
+            actual = one_away(test_s1, test_s2)
+            self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
