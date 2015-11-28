9ef25449e540fcb563531c15aad375b2ecd8fc4e
Leeor Engel <leeorengel@gmail.com>
2015-11-28 13:38:38 -0500

    added chapter 4 q5 solutions

---
+++ Clojure/src/chapter_4/chapter_4_q5.clj
@@ -0,0 +1,15 @@
+(ns chapter-4.chapter-4-q5
+  (:require [data-structures.tree :refer :all]))
+
+;; O(n)
+(defn valid-bst? [tree]
+  (if (some? tree)
+    (cond (and (some? (:left tree)) (< (:data tree) (:data (:left tree)))) false
+          (and (some? (:right tree)) (> (:data tree) (:data (:right tree)))) false
+          :else (and (valid-bst? (:left tree)) (valid-bst? (:right tree))))
+    true))
+
+;; Worse time-complexity O(n log n)
+(defn valid-bst-alt? [tree]
+  (let [in-order-traversal (in-order-walk tree)]
+    (= (sort in-order-traversal) in-order-traversal)))
---
+++ Clojure/test/chapter_4/chapter_4_q5_test.clj
@@ -0,0 +1,56 @@
+(ns chapter-4.chapter-4-q5-test
+  (:require [clojure.test :refer :all]
+            [data-structures.tree :refer :all]
+            [chapter-4.chapter-4-q5 :refer :all]))
+
+(deftest valid-bst-test
+  (testing "single node"
+    (let [tree (create-tree [1])]
+      (is (valid-bst? tree))))
+
+  (testing "two nodes"
+    (let [tree-1 (create-tree [1 [2]])
+          tree-2 (create-tree [1 [0]])
+          tree-3 (create-tree [1 [] [2]])]
+      (is (not (valid-bst? tree-1)))
+      (is (valid-bst? tree-2))
+      (is (valid-bst? tree-3))))
+
+  (testing "balanced > 2 nodes"
+    (let [tree-1 (create-tree [2 [1] [3]])
+          tree-2 (create-tree [2
+                               [1] [4
+                                    [3]]])
+          tree-3 (create-tree [2
+                               [1] [3
+                                    [4]]])]
+      (is (valid-bst? tree-1))
+      (is (valid-bst? tree-2))
+      (is (not (valid-bst? tree-3))))))
+
+(deftest valid-bst-alt-test
+  (testing "single node"
+    (let [tree (create-tree [1])]
+      (is (valid-bst-alt? tree))))
+
+  (testing "two nodes"
+    (let [tree-1 (create-tree [1 [2]])
+          tree-2 (create-tree [1 [0]])
+          tree-3 (create-tree [1 [] [2]])]
+      (is (not (valid-bst-alt? tree-1)))
+      (is (valid-bst-alt? tree-2))
+      (is (valid-bst-alt? tree-3))))
+
+  (testing "balanced > 2 nodes"
+    (let [tree-1 (create-tree [2 [1] [3]])
+          tree-2 (create-tree [2
+                               [1] [4
+                                    [3]]])
+          tree-3 (create-tree [2
+                               [1] [3
+                                    [4]]])]
+      (is (valid-bst-alt? tree-1))
+      (is (valid-bst-alt? tree-2))
+      (is (not (valid-bst-alt? tree-3))))))
+
+(run-tests)
