6eacd6115ebeffac006c05d9dd8b20cf8686b6dd
Leeor Engel <leeorengel@gmail.com>
2015-11-30 08:39:27 -0500

    ch2 q1 - updated to use built-in linked list type

--- Clojure/src/chapter_2/chapter_2_q1.clj
+++ Clojure/src/chapter_2/chapter_2_q1.clj
@@ -1,46 +1,24 @@
-(ns chapter-2.chapter-2-q1
-  (:require
-    [data-structures.linked-list :refer :all])
-  (:import
-    (clojure.lang Atom)
-    (data_structures.linked_list SinglyLinkedList)))
+(ns chapter-2.chapter-2-q1)
 
-(defn- remove-node [^Atom prev ^Atom n]
-  {:pre [(some? @n)]}
-  "Removes the node n from the linked list"
-  (let [^Atom next (:next @n)]
-    (if (some? next)
-      (reset! (:next @prev) @next)
-      (reset! prev (create-node (:data @prev))))))
+;;
+;; Implementations using clojure's built-in list type, which is a singly-linked list.
+;;
+
+(defn create-linked-list [spec-vec]
+  (into '() (reverse spec-vec)))
 
 ;; O(n) time, O(n) space
-(defn remove-dupes-with-buffer [^SinglyLinkedList l]
-  (loop [seen-nodes #{}
-         ^Atom prev nil
-         ^Atom n (:head l)]
-    (if (nil? n)
-      l
-      (if (contains? seen-nodes (:data @n))
-        (do (remove-node prev n)
-            (recur seen-nodes
-                   prev
-                   (:next @prev)))
-        (recur (conj seen-nodes (:data @n))
-               n
-               (:next @n))))))
+(defn remove-dupes-with-buffer [l]
+  (second (reduce (fn [[seen deduped] node]
+                    (if (contains? seen node)
+                      [seen deduped]
+                      [(conj seen node) (conj deduped node)]))
+                  [#{} '()]
+                  (reverse l))))
 
 ;; O(n^2)
-(defn remove-dupes [^SinglyLinkedList l]
-  (loop [^Atom n (:head l)]
-    (if (nil? n)
-      l
-      (recur (loop [^Atom prev n
-                    ^Atom m (:next @n)]
-               (if (nil? m)
-                 (:next @n)
-                 (if (= (:data @n) (:data @m))
-                   (do (remove-node prev m)
-                       (recur prev
-                              (:next @prev)))
-                   (recur m
-                          (:next @m)))))))))
+(defn remove-dupes [l]
+  (reduce (fn [deduped node]
+            (conj (remove #(= node %) deduped) node))
+          '()
+          (reverse l)))
