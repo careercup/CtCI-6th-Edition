e41639361779cad5bc5e04f8662a2cdbae03babe
Leeor Engel <leeorengel@gmail.com>
2015-11-30 22:10:02 -0500

    greatly simplified q6 solution

--- Clojure/src/chapter_2/chapter_2_q6.clj
+++ Clojure/src/chapter_2/chapter_2_q6.clj
@@ -1,16 +1,5 @@
 (ns ^{:author "Leeor Engel"}
-  chapter-2.chapter-2-q6
-  (:require
-    [data-structures.mutable-linked-list :refer :all])
-  (:import (clojure.lang Atom)
-           (data_structures.mutable_linked_list SinglyLinkedList)))
+  chapter-2.chapter-2-q6)
 
-(defn palindrome? [^SinglyLinkedList l]
-  (loop [^SinglyLinkedList reversed nil
-         ^Atom n (:head l)]
-    (if (nil? n)
-      (lists-eq? reversed l)
-      (recur (if (nil? reversed)
-               (create-linked-list (:data @n))
-               (add-node reversed (:data @n)))
-             (:next @n)))))
+(defn palindrome? [l]
+  (= l (reverse l)))
