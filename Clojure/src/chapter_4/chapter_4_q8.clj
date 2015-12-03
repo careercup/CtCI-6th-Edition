be7e76b7232f83868ed264c5c8d4f7eecec1efdd
Leeor Engel <leeorengel@gmail.com>
2015-12-02 23:21:31 -0500

    clarified argument types

--- Clojure/src/chapter_4/chapter_4_q8.clj
+++ Clojure/src/chapter_4/chapter_4_q8.clj
@@ -17,7 +17,7 @@ chapter-4.chapter-4-q8
                   n1-parent-node
                   (first-common-ancestor-helper n1-parent n1-new-depth n2-parent n2-new-depth)))))
 
-(defn first-common-ancestor [n1 n2]
-  (let [n1-depth (count (zip/path n1))
-        n2-depth (count (zip/path n2))]
-    (first-common-ancestor-helper n1 n1-depth n2 n2-depth)))
+(defn first-common-ancestor [zipper-n1 zipper-n2]
+  (let [n1-depth (count (zip/path zipper-n1))
+        n2-depth (count (zip/path zipper-n2))]
+    (first-common-ancestor-helper zipper-n1 n1-depth zipper-n2 n2-depth)))
