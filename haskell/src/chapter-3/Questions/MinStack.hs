c28e3bfa19e5d690d1c5992770116e168e22ad7b
Noah Sluss <slussnoah@gmail.com>
2015-12-15 23:59:23 -0800

    complete MinStack prompt

--- haskell/src/chapter-3/Questions/MinStack.hs
+++ haskell/src/chapter-3/Questions/MinStack.hs
@@ -9,19 +9,23 @@ import qualified Data.Stack as S
 data MinStack a = MinStack (S.Stack a) (S.Stack a)
   deriving (Eq, Show)
 
-push :: a -> MinStack a -> MinStack a
-push = error "implement push"
---push a (MinStack s1 (S.Stack [])) = MinStack (S.push a s1) (S.mkStack a)
---push a (MinStack s1 s2@(S.Stack (x:xs))) =
---  if a <= x
---  then MinStack (S.push a s1) (S.push a s2)
---  else MinStack (S.push a s1) s2
+push :: Ord a => a -> MinStack a -> MinStack a
+push a (MinStack s1 (S.Stack [])) = MinStack (S.push a s1) (S.mkStack a)
+push a (MinStack s1 s2@(S.Stack (x:xs))) =
+  if a <= x
+  then MinStack (S.push a s1) (S.push a s2)
+  else MinStack (S.push a s1) s2
 
-pop :: MinStack a -> MinStack a
-pop = error "implement pop"
+pop :: Ord a => MinStack a -> MinStack a
+pop ms@(MinStack s1 s2) = case (S.peek s1, S.peek s2) of
+  (Just a, Just b) -> if a > b 
+                      then MinStack (S.pop s1) s2
+                      else MinStack (S.pop s1) (S.pop s2)
+  otherwise -> ms
+
 
 smin :: MinStack a -> Maybe a
-smin = error "implement min"
+smin (MinStack _ s2) = S.peek s2
 
 mkMinStack :: a -> MinStack a
 mkMinStack a = let st = S.mkStack a
