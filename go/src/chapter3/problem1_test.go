98b10c12758433d3ecf704eb381439ffe66bce5d
Lee Woodridge <le3wood@gmail.com>
2016-01-30 10:52:59 -0800

    Improved multi stack tests to check for errors.

--- go/src/chapter3/problem1_test.go
+++ go/src/chapter3/problem1_test.go
@@ -6,12 +6,14 @@ import (
 )
 
 func TestSliceMultiStack(t *testing.T) {
+	// Set up data.
 	vals1 := []int{1, 2, 3, 4, 5}
 	vals2 := []int{6, 7, 8}
 	vals3 := []int{9, 10}
 	expected := []int{1, 2, 3, 4, 5, 0, 6, 7, 8,
 		0, 0, 0, 9, 10, 0, 0, 0, 0}
 	s := GetSliceMultiStack(3, 6)
+	// Test push.
 	for _, val := range vals1 {
 		s.Push(1, val)
 	}
@@ -32,9 +34,20 @@ func TestSliceMultiStack(t *testing.T) {
 	if !reflect.DeepEqual(actual, expected) {
 		t.Fatalf("Expected: %v, actual: %v\n", expected, actual)
 	}
-	s.Pop(1)
-	s.Pop(1)
-	s.Pop(3)
+	// Test pop.
+	var err error
+	pop1, err := s.Pop(1)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v\n", err)
+	}
+	pop2, err := s.Pop(1)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v\n", err)
+	}
+	pop3, err := s.Pop(3)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v\n", err)
+	}
 	expected = []int{1, 2, 3, 0, 0, 0, 6, 7, 8,
 		0, 0, 0, 9, 0, 0, 0, 0, 0}
 	actual = []int{}
@@ -46,6 +59,11 @@ func TestSliceMultiStack(t *testing.T) {
 		}
 	}
 	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("Expected: %v, actual: %v\n", expected, actual)
+	}
+	pops := []int{pop1, pop2, pop3}
+	expected = []int{5, 4, 10}
+	if !reflect.DeepEqual(pops, expected) {
 		t.Fatalf("Expected: %v, actual: %v\n", expected, actual)
 	}
 }
