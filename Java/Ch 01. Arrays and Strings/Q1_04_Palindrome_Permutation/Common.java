d5330e1051eeccffd31f4aa86564cfb069432057
Gayle McDowell <gayle@careercup.com>
2016-02-03 12:52:50 -0800

    Removing unnecessary code

--- Java/Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/Common.java
+++ Java/Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/Common.java
@@ -5,14 +5,10 @@ public class Common {
 	public static int getCharNumber(Character c) {
 		int a = Character.getNumericValue('a');
 		int z = Character.getNumericValue('z');
-		int A = Character.getNumericValue('A');
-		int Z = Character.getNumericValue('Z');
 		
 		int val = Character.getNumericValue(c);
 		if (a <= val && val <= z) {
 			return val - a;
-		} else if (A <= val && val <= Z) {
-			return val - A;
 		}
 		return -1;
 	}
