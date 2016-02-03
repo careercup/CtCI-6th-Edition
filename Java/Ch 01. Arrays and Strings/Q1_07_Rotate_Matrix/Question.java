f046cfbb28a631b4f01793eaef3648c3cbdf4618
Gayle McDowell <gayle@careercup.com>
2016-02-03 12:53:03 -0800

    Adding error checking on 1.7

--- Java/Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/Question.java
+++ Java/Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/Question.java
@@ -4,11 +4,14 @@ import CtCILibrary.*;
 
 public class Question {
 
-	public static void rotate(int[][] matrix, int n) {
-		for (int layer = 0; layer < n / 2; ++layer) {
+	public static boolean rotate(int[][] matrix) {
+		if (matrix.length == 0 || matrix.length != matrix[0].length) return false; // Not a square
+		int n = matrix.length;
+		
+		for (int layer = 0; layer < n / 2; layer++) {
 			int first = layer;
 			int last = n - 1 - layer;
-			for(int i = first; i < last; ++i) {
+			for(int i = first; i < last; i++) {
 				int offset = i - first;
 				int top = matrix[first][i]; // save top
 
@@ -25,12 +28,13 @@ public class Question {
 				matrix[i][last] = top; // right <- saved top
 			}
 		}
+		return true;
 	}
 	
 	public static void main(String[] args) {
-		int[][] matrix = AssortedMethods.randomMatrix(10, 10, 0, 9);
+		int[][] matrix = AssortedMethods.randomMatrix(3, 3, 0, 9);
 		AssortedMethods.printMatrix(matrix);
-		rotate(matrix, 10);
+		rotate(matrix);
 		System.out.println();
 		AssortedMethods.printMatrix(matrix);
 	}
