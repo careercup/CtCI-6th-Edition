b3e730e17c57714f062ab975d8ae28e38f6c07fc
Gayle McDowell <gayle@careercup.com>
2016-02-04 15:22:11 -0800

    Adding comment about infinite slope handling

--- Java/Ch 16. Moderate/Q16_03_Intersection/Line.java
+++ Java/Ch 16. Moderate/Q16_03_Intersection/Line.java
@@ -7,7 +7,7 @@ public class Line {
 	public Line(Point start, Point end) {
 		double deltaY = end.y - start.y;
 		double deltaX = end.x - start.x;
-		slope = deltaY / deltaX;
+		slope = deltaY / deltaX; // Will be Infinity (not exception) when deltaX = 0 
 		yintercept = end.y - slope * end.x;
 	}
 		
