35d4a8ec3bfd303872dd2919843ed0d4bbccfceb
Gayle McDowell <gayle@careercup.com>
2016-02-04 23:16:06 -0800

    Removing unnecessary if statements in 8.9 (parens)

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_09_Parens/QuestionB.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_09_Parens/QuestionB.java
@@ -4,21 +4,17 @@ import java.util.ArrayList;
 
 public class QuestionB {
 	
-	public static void addParen(ArrayList<String> list, int leftRem, int rightRem, char[] str, int count) {
+	public static void addParen(ArrayList<String> list, int leftRem, int rightRem, char[] str, int index) {
 		if (leftRem < 0 || rightRem < leftRem) return; // invalid state
 		
 		if (leftRem == 0 && rightRem == 0) { /* all out of left and right parentheses */
-			String s = String.copyValueOf(str);
-			list.add(s);
+			list.add(String.copyValueOf(str));
 		} else {
-			if (leftRem > 0) { // try a left paren, if there are some available
-				str[count] = '(';
-				addParen(list, leftRem - 1, rightRem, str, count + 1);
-			}
-			if (rightRem > leftRem) { // try a right paren, if there�s a matching left
-				str[count] = ')';
-				addParen(list, leftRem, rightRem - 1, str, count + 1);
-			}
+			str[index] = '('; // Add left and recurse
+			addParen(list, leftRem - 1, rightRem, str, index + 1);
+			
+			str[index] = ')'; // Add right and recurse
+			addParen(list, leftRem, rightRem - 1, str, index + 1);
 		}
 	}
 	
@@ -30,7 +26,7 @@ public class QuestionB {
 	}
 	
 	public static void main(String[] args) {
-		ArrayList<String> list = generateParens(3);
+		ArrayList<String> list = generateParens(6);
 		for (String s : list) {
 			System.out.println(s);
 		}
