038f6fef58b0f50fe6da856008f63b42f15278c5
Gayle McDowell <gayle@careercup.com>
2016-02-10 14:55:27 -0800

    Changed approach to middle solution to better handle duplicates

--- Java/Ch 16. Moderate/Q16_24_Pairs_With_Sum/QuestionB.java
+++ Java/Ch 16. Moderate/Q16_24_Pairs_With_Sum/QuestionB.java
@@ -1,25 +1,33 @@
 package Q16_24_Pairs_With_Sum;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
-public class QuestionB {	
+public class QuestionB {
 	public static ArrayList<Pair> printPairSums(int[] array, int sum) {
 		ArrayList<Pair> result = new ArrayList<Pair>();
-		HashSet<Integer> elements = new HashSet<Integer>();
+		HashMap<Integer, Integer> unpairedCount = new HashMap<Integer, Integer>();
 		for (int x : array) {
 			int complement = sum - x;
-			if (elements.contains(complement) && !elements.contains(x)) {
+			if (unpairedCount.getOrDefault(complement, 0) > 0) {
 				result.add(new Pair(x, complement));
+				adjustCounterBy(unpairedCount, complement, -1); // decrement complement
+			} else {
+				adjustCounterBy(unpairedCount, x, 1); // increment x
 			}
-			elements.add(x);
 		}
 		return result;
 	}
 	
+	public static void adjustCounterBy(HashMap<Integer, Integer> counter, int key, int delta) {
+		counter.put(key, counter.getOrDefault(key, 0) + delta);
+	}
+	
+	
 	public static void main(String[] args) {
-		int[] test = {9, 3, 6, 5, 7, 7, -1, 13, 14, -2, 12, 0};
-		ArrayList<Pair> pairs = printPairSums(test, 12);
+		int[] test = {-1, -1, -1, -1, 0, 0, 0, 0, 1, 1};
+		ArrayList<Pair> pairs = printPairSums(test, -1);
 		for (Pair p : pairs) {
 			System.out.println(p.toString());
 		}
