15ae740a87733fd58b9bf3006c7ebb837ab78102
Gayle McDowell <gayle@careercup.com>
2016-02-09 17:26:30 -0800

    Separating solutions into two different files

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_04_Power_Set/QuestionA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_04_Power_Set/QuestionA.java
@@ -2,7 +2,7 @@ package Q8_04_Power_Set;
 
 import java.util.*;
 
-public class Question {
+public class QuestionA {
 
 	public static ArrayList<ArrayList<Integer>> getSubsets(ArrayList<Integer> set, int index) {
 		ArrayList<ArrayList<Integer>> allsubsets;
@@ -15,33 +15,11 @@ public class Question {
 			ArrayList<ArrayList<Integer>> moresubsets = new ArrayList<ArrayList<Integer>>();
 			for (ArrayList<Integer> subset : allsubsets) {
 				ArrayList<Integer> newsubset = new ArrayList<Integer>();
-				newsubset.addAll(subset); //
+				newsubset.addAll(subset); 
 				newsubset.add(item);
 				moresubsets.add(newsubset);
 			}
 			allsubsets.addAll(moresubsets);
-		}
-		return allsubsets;
-	}
-	
-	public static ArrayList<Integer> convertIntToSet(int x, ArrayList<Integer> set) {
-		ArrayList<Integer> subset = new ArrayList<Integer>(); 
-		int index = 0;
-		for (int k = x; k > 0; k >>= 1) {
-			if ((k & 1) == 1) {
-				subset.add(set.get(index));
-			}
-			index++;
-		}
-		return subset;
-	}
-	
-	public static ArrayList<ArrayList<Integer>> getSubsets2(ArrayList<Integer> set) {
-		ArrayList<ArrayList<Integer>> allsubsets = new ArrayList<ArrayList<Integer>>();
-		int max = 1 << set.size(); /* Compute 2^n */ 
-		for (int k = 0; k < max; k++) {
-			ArrayList<Integer> subset = convertIntToSet(k, set);
-			allsubsets.add(subset);
 		}
 		return allsubsets;
 	}
@@ -52,10 +30,7 @@ public class Question {
 			list.add(i);
 		}
 		ArrayList<ArrayList<Integer>> subsets = getSubsets(list, 0);
-		System.out.println(subsets.toString());
-		
-		ArrayList<ArrayList<Integer>> subsets2 = getSubsets2(list);
-		System.out.println(subsets2.toString());		
+		System.out.println(subsets.toString());	
 	}
 
 }
