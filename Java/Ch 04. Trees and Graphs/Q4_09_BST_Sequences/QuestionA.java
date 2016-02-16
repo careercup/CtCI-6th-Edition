1c59e9a682a1a5006d357860878fb6dd1245ab99
Pap Lőrinc <paplorinc@yahoo.com>
2016-02-16 22:18:14 +0200

    Added alternative Java solution to Q4_09

--- Java/Ch 04. Trees and Graphs/Q4_09_BST_Sequences/QuestionA.java
+++ Java/Ch 04. Trees and Graphs/Q4_09_BST_Sequences/QuestionA.java
@@ -1,17 +1,17 @@
 package Q4_09_BST_Sequences;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 import CtCILibrary.TreeNode;
 
-public class Question {
-	
-	public static void weaveLists(LinkedList<Integer> first, LinkedList<Integer> second, ArrayList<LinkedList<Integer>> results, LinkedList<Integer> prefix) {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class QuestionA {
+	private static void weaveLists(LinkedList<Integer> first, LinkedList<Integer> second, List<LinkedList<Integer>> results, LinkedList<Integer> prefix) {
 		/* One list is empty. Add the remainder to [a cloned] prefix and
 		 * store result. */
 		if (first.size() == 0 || second.size() == 0) {
-			LinkedList<Integer> result = (LinkedList<Integer>) prefix.clone();
+			LinkedList<Integer> result = new LinkedList<>(prefix);
 			result.addAll(first);
 			result.addAll(second);
 			results.add(result);
@@ -32,29 +32,29 @@ public class Question {
 		int headSecond = second.removeFirst();
 		prefix.addLast(headSecond);
 		weaveLists(first, second, results, prefix);
-		prefix.removeLast();	
+		prefix.removeLast();
 		second.addFirst(headSecond);
 	}
-	
-	public static ArrayList<LinkedList<Integer>> allSequences(TreeNode node) {
-		ArrayList<LinkedList<Integer>> result = new ArrayList<LinkedList<Integer>>();
-		
+
+	public static List<LinkedList<Integer>> allSequences(TreeNode node) {
+		List<LinkedList<Integer>> result = new ArrayList<>();
+
 		if (node == null) {
-			result.add(new LinkedList<Integer>());
+			result.add(new LinkedList<>());
 			return result;
-		} 
-		
-		LinkedList<Integer> prefix = new LinkedList<Integer>();
+		}
+
+		LinkedList<Integer> prefix = new LinkedList<>();
 		prefix.add(node.data);
 		
 		/* Recurse on left and right subtrees. */
-		ArrayList<LinkedList<Integer>> leftSeq = allSequences(node.left);
-		ArrayList<LinkedList<Integer>> rightSeq = allSequences(node.right);
+		List<LinkedList<Integer>> leftSeq = allSequences(node.left);
+		List<LinkedList<Integer>> rightSeq = allSequences(node.right);
 		
 		/* Weave together each list from the left and right sides. */
 		for (LinkedList<Integer> left : leftSeq) {
 			for (LinkedList<Integer> right : rightSeq) {
-				ArrayList<LinkedList<Integer>> weaved = new ArrayList<LinkedList<Integer>>();
+				List<LinkedList<Integer>> weaved = new ArrayList<>();
 				weaveLists(left, right, weaved, prefix);
 				result.addAll(weaved);
 			}
@@ -64,15 +64,14 @@ public class Question {
 
 	public static void main(String[] args) {
 		TreeNode node = new TreeNode(100);
-		int[] array = {100, 50, 20, 75, 150, 120, 170};
+		int[] array = {50, 20, 75, 150, 120, 170};
 		for (int a : array) {
 			node.insertInOrder(a);
 		}
-		ArrayList<LinkedList<Integer>> allSeq = allSequences(node);
-		for (LinkedList<Integer> list : allSeq) {
+		List<LinkedList<Integer>> allSeq = allSequences(node);
+		for (List<Integer> list : allSeq) {
 			System.out.println(list);
 		}
 		System.out.println(allSeq.size());
 	}
-
 }
