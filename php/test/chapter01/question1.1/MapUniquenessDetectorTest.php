60db8702e17aec119ee6b9d3466d95bf6d2d8801
asaph <ace@dontletgo.com>
2016-02-17 23:34:54 -0800

    Removing php folder from main repo

--- php/test/chapter01/question1.1/MapUniquenessDetectorTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../src/chapter01/question1.1/MapUniquenessDetector.php';
-
-class MapUniquenessDetectorTest extends PHPUnit_Framework_TestCase {
-    public function testIsUnique() {
-        $this->assertTrue(MapUniquenessDetector::isUnique("abcdefg"));
-        $this->assertFalse(MapUniquenessDetector::isUnique("level"));
-    }
-}
