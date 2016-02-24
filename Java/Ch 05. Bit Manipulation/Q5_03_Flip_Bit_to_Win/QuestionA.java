c65904ce03b476f7d6f6ecd3626e31c6ae091f91
Pap Lőrinc <paplorinc@yahoo.com>
2016-02-24 17:51:36 +0200

    Simplified Java solutions for `Q5_03`

--- Java/Ch 05. Bit Manipulation/Q5_03_Flip_Bit_to_Win/QuestionA.java
+++ Java/Ch 05. Bit Manipulation/Q5_03_Flip_Bit_to_Win/QuestionA.java
@@ -1,27 +1,24 @@
 package Q5_03_Flip_Bit_to_Win;
 
 public class QuestionA {
-	
-	public static int SEQUENCE_LENGTH = 32;
-	
-	public static boolean getBit(int num, int i) {
+	private static boolean getBit(int num, int i) {
 		return ((num & (1 << i)) != 0);
-	}	
-	
+	}
+
 	public static int longestSequence(int n) {
 		int maxSeq = 0;
-		
-		for (int i = 0; i < SEQUENCE_LENGTH; i++) {
-			maxSeq = Math.max(maxSeq,  longestSequenceOf1s(n, i));
+
+		for (int i = 0; i < Integer.SIZE; i++) {
+			maxSeq = Math.max(maxSeq, longestSequenceOf1s(n, i));
 		}
-		
+
 		return maxSeq;
 	}
-	
-	public static int longestSequenceOf1s(int n, int indexToIgnore) {
+
+	private static int longestSequenceOf1s(int n, int indexToIgnore) {
 		int max = 0;
 		int counter = 0;
-		for (int i = 0; i < SEQUENCE_LENGTH; i++) {
+		for (int i = 0; i < Integer.SIZE; i++) {
 			if (i == indexToIgnore || getBit(n, i)) {
 				counter++;
 				max = Math.max(counter, max);
@@ -30,14 +27,13 @@ public class QuestionA {
 			}
 		}
 		return max;
-	}	
-	
+	}
+
 	public static void main(String[] args) {
 		int original_number = Integer.MAX_VALUE;
 		int new_number = longestSequence(original_number);
-			
-		System.out.println(Integer.toBinaryString(original_number));
-		System.out.println(new_number);			
-	}
 
+		System.out.println(Integer.toBinaryString(original_number));
+		System.out.println(new_number);
+	}
 }
--- Java/Ch 05. Bit Manipulation/Q5_03_Flip_Bit_to_Win/QuestionC.java
+++ Java/Ch 05. Bit Manipulation/Q5_03_Flip_Bit_to_Win/QuestionC.java
@@ -1,38 +1,35 @@
 package Q5_03_Flip_Bit_to_Win;
 
 public class QuestionC {
-
-	public static int SEQUENCE_LENGTH = 32;
-	
 	/* Given set of three sequences ordered as {0s, then 1s, then 0s}, 
 	 * find max sequence that can be formed. */
-	public static int getMaxSequence(int[] sequences) { /* 1s, then 0s, then [old] ones */
+	private static int getMaxSequence(int[] sequences) { /* 1s, then 0s, then [old] ones */
 		if (sequences[1] == 1) { // a single 0 -> merge sequences
-			return sequences[0] + sequences[2] + 1; 
+			return sequences[0] + sequences[2] + 1;
 		} else if (sequences[1] == 0) { // no 0s -> take one side
-			return Math.max(sequences[0], sequences[2]); 
+			return Math.max(sequences[0], sequences[2]);
 		} else {  // many 0s -> take side, add 1 (flip a bit)
 			return Math.max(sequences[0], sequences[2]) + 1;
 		}
 	}
-	
-	public static void shift(int[] sequences) {
+
+	private static void shift(int[] sequences) {
 		sequences[2] = sequences[1];
 		sequences[1] = sequences[0];
 		sequences[0] = 0;
 	}
-	
+
 	public static int longestSequence(int n) {
 		int searchingFor = 0;
 		int[] sequences = {0, 0, 0}; // Counts of last 3 sequences
 		int maxSequence = 1;
-		
-		for (int i = 0; i < SEQUENCE_LENGTH; i++) {
+
+		for (int i = 0; i < Integer.SIZE; i++) {
 			if ((n & 1) != searchingFor) {
 				if (searchingFor == 1) { // End of 1s + 0s + 1s sequence
 					maxSequence = Math.max(maxSequence, getMaxSequence(sequences));
 				}
-				
+
 				searchingFor = n & 1; // Flip 1 to 0 or 0 to 1
 				shift(sequences); // Shift sequences
 			}
@@ -42,20 +39,16 @@ public class QuestionC {
 		
 		/* Check final set of sequences */
 		if (searchingFor == 0) {
-			shift(sequences);		
+			shift(sequences);
 		}
-		int finalSequence = getMaxSequence(sequences);
-		maxSequence = Math.max(finalSequence, maxSequence);		
-		
-		return maxSequence;
-	}		
-	
+		return Math.max(maxSequence, getMaxSequence(sequences));
+	}
+
 	public static void main(String[] args) {
 		int original_number = Integer.MAX_VALUE;
 		int new_number = longestSequence(original_number);
-			
-		System.out.println(Integer.toBinaryString(original_number));
-		System.out.println(new_number);		
-	}
 
+		System.out.println(Integer.toBinaryString(original_number));
+		System.out.println(new_number);
+	}
 }
