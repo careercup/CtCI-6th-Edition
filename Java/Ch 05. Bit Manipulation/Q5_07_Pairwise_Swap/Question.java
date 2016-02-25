2cacd377610afd2bba5b76ca014859a56af31569
Pap Lőrinc <paplorinc@yahoo.com>
2016-02-25 23:16:25 +0200

    Simplified Java solution for `Q5_07`

--- Java/Ch 05. Bit Manipulation/Q5_07_Pairwise_Swap/Question.java
+++ Java/Ch 05. Bit Manipulation/Q5_07_Pairwise_Swap/Question.java
@@ -3,16 +3,16 @@ package Q5_07_Pairwise_Swap;
 import CtCILibrary.AssortedMethods;
 
 public class Question {
+	private final static int ODD_MASK = 0b01010101_01010101_01010101_01010101;
 
-	public static int swapOddEvenBits(int x) { 
-		return ( ((x & 0xaaaaaaaa) >>> 1) | ((x & 0x55555555) << 1) ); 
+	public static int swapOddEvenBits(int num) {
+		return ((num >> 1) & ODD_MASK) | ((num & ODD_MASK) << 1);
 	}
-	
+
 	public static void main(String[] args) {
 		int a = 234321;
 		System.out.println(a + ": " + AssortedMethods.toFullBinaryString(a));
 		int b = swapOddEvenBits(a);
 		System.out.println(b + ": " + AssortedMethods.toFullBinaryString(b));
 	}
-
 }
