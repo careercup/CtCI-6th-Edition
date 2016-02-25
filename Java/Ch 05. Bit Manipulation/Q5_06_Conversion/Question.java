0cf9b319f0da85f4be1c9a72e27fa36cce68ee58
Pap Lőrinc <paplorinc@yahoo.com>
2016-02-25 22:39:39 +0200

    Added alternative Java solution to `Q5_06`

--- Java/Ch 05. Bit Manipulation/Q5_06_Conversion/Question.java
+++ Java/Ch 05. Bit Manipulation/Q5_06_Conversion/Question.java
@@ -5,20 +5,24 @@ import CtCILibrary.AssortedMethods;
 public class Question {
 	public static int bitSwapRequired(int a, int b) {
 		int count = 0;
-		for (int c = a ^ b; c != 0; c = c >> 1) { 
+		for (int c = a ^ b; c != 0; c = c >> 1) {
 			count += c & 1;
 		}
 		return count;
 	}
-	
-	public static int bitSwapRequired2(int a, int b){
+
+	public static int bitSwapRequired2(int a, int b) {
 		int count = 0;
-		for (int c = a ^ b; c != 0; c = c & (c-1)) {
+		for (int c = a ^ b; c != 0; c = c & (c - 1)) {
 			count++;
 		}
 		return count;
 	}
-	
+
+	public static int bitSwapRequired3(int a, int b) {
+		return Integer.bitCount(a ^ b);
+	}
+
 	public static void main(String[] args) {
 		int a = 23432;
 		int b = 512132;
@@ -26,6 +30,7 @@ public class Question {
 		System.out.println(b + ": " + AssortedMethods.toFullBinaryString(b));
 		int nbits = bitSwapRequired(a, b);
 		int nbits2 = bitSwapRequired2(a, b);
-		System.out.println("Required number of bits: " + nbits + " " + nbits2);
+		int nbits3 = bitSwapRequired3(a, b);
+		System.out.println("Required number of bits: " + nbits + " " + nbits2 + " " + nbits3);
 	}
 }
