a90dc7f7f78e445fc62a43ceb2527d552b686969
Gayle McDowell <gayle@careercup.com>
2016-06-12 15:42:16 -0700

    minor change to make method private

--- Java/Ch 10. Sorting and Searching/Q10_02_Group_Anagrams/AnagramComparator.java
+++ Java/Ch 10. Sorting and Searching/Q10_02_Group_Anagrams/AnagramComparator.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class AnagramComparator implements Comparator<String> {
-	public String sortChars(String s) {
+	private String sortChars(String s) {
 		char[] content = s.toCharArray();
 		Arrays.sort(content);
 		return new String(content);
