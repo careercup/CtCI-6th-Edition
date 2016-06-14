19a790903ad3ec7c18355c86fd5f2bb981b1ff19
Gayle McDowell <gayle@careercup.com>
2016-06-14 00:45:18 -0700

    5.06 Changed from arithmetic to logical shift to handle negative numbers

--- Java/Ch 05. Bit Manipulation/Q5_06_Conversion/Question.java
+++ Java/Ch 05. Bit Manipulation/Q5_06_Conversion/Question.java
@@ -5,7 +5,7 @@ import CtCILibrary.AssortedMethods;
 public class Question {
 	public static int bitSwapRequired(int a, int b) {
 		int count = 0;
-		for (int c = a ^ b; c != 0; c = c >> 1) { 
+		for (int c = a ^ b; c != 0; c = c >>> 1) { 
 			count += c & 1;
 		}
 		return count;
@@ -20,7 +20,7 @@ public class Question {
 	}
 	
 	public static void main(String[] args) {
-		int a = 23432;
+		int a = -23432;
 		int b = 512132;
 		System.out.println(a + ": " + AssortedMethods.toFullBinaryString(a));
 		System.out.println(b + ": " + AssortedMethods.toFullBinaryString(b));
