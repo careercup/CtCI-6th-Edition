401861fa9abdf709ffad88d4a0029da8ed3a278b
Mohammad <momosharif58@gmail.com>
2017-01-16 16:22:15 -0800

    Modified URLify, cleaner and more compact approach

--- Java/Ch 01. Arrays and Strings/Q1_03_URLify/Question.java
+++ Java/Ch 01. Arrays and Strings/Q1_03_URLify/Question.java
@@ -4,42 +4,34 @@ import CtCILibrary.AssortedMethods;
 
 public class Question {
 	// Assume string has sufficient free space at the end
-	public static void replaceSpaces(char[] str, int trueLength) {
-		int spaceCount = 0, index, i = 0;
-		for (i = 0; i < trueLength; i++) {
-			if (str[i] == ' ') {
-				spaceCount++;
-			}
-		}
-		index = trueLength + spaceCount * 2;
-		if (trueLength < str.length) str[trueLength] = '\0';
-		for (i = trueLength - 1; i >= 0; i--) {
-			if (str[i] == ' ') {
-				str[index - 1] = '0';
-				str[index - 2] = '2';
-				str[index - 3] = '%';
-				index = index - 3;
-			} else {
-				str[index - 1] = str[i];
-				index--;
-			}
-		}
-	}
-	
-	public static int findLastCharacter(char[] str) {
-		for (int i = str.length - 1; i >= 0; i--) {
-			if (str[i] != ' ') {
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	public static void main(String[] args) {
-		String str = "Mr John Smith    ";
-		char[] arr = str.toCharArray();
-		int trueLength = findLastCharacter(arr) + 1;
-		replaceSpaces(arr, trueLength);	
-		System.out.println("\"" + AssortedMethods.charArrayToString(arr) + "\"");
-	}
+	public static void main(String[] args){
+	        System.out.println("Expected: Mr%20John%20Smith");
+	        System.out.print("Result: ");
+	        URLify("Mr John Smith    ", 13);
+	        System.out.println("Expected: What%20A%20Wonderful%20Day");
+	        System.out.print("Result: ");
+	        URLify("What A Wonderful Day      ", 20);
+	        System.out.println("Expected: I%20Never%20Asked%20For%20Help");
+	        System.out.print("Result: ");
+	        URLify("I Never Asked For Help        ", 22);
+	    }
+
+	    public static void URLify(String sentence, int trueLength){
+	        char[] string = sentence.toCharArray();             // Begin by creating a char array using the given sentence string
+	        int decrementer = 0;                                // Initially set a decrementer to 0 which will later move our pointer
+	        int pointer = string.length - 1;                    // Set the pointer to the end of the array so that characters can be shifted over
+	        for(int i = trueLength - 1; i >= 0; i--){           // i is essentially our other pointer which begins where the last character in the sentence is
+	            if(' ' == string[i]){                           // If the character is a space...
+	                string[pointer - decrementer] = '0';        // add a 0 to the left of our pointer near the end of the array
+	                string[pointer - decrementer - 1] = '2';    // add a 2 to the left of the 0
+	                string[pointer - decrementer - 2] = '%';    // add a % to the left of the 2
+	                decrementer += 3;                           // Now since our pointer is still at the index to the right of the 0, we need to shift left 3 spaces
+	            } else {                                        // This leaves our pointer at the next available space for character storage
+	                string[pointer - decrementer] = string[i];  // Insert the character that i points to, in the space our pointer is pointing to
+	                decrementer++;                              // Decrement our decrementer
+	            }
+	        }
+	        String formatted = new String(string);              // Form a string from our newly formatted char[]
+	        System.out.println(formatted);                      // Print the string to the console
+	    }
 }
