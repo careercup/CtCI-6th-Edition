ea43afe675167ae4b583abf8a216fd8fa6257a22
Roland Illig <roland.illig@gmx.de>
2017-06-30 20:45:02 +0200

    Q7_01_Deck_of_Cards: write errors to System.err
    Error messages belong to System.err instead of System.out. Furthermore, the program should communicate the failure via returning an exit code.
    
    While here, provided early return paths in case of errors.

--- Java/Ch 07. Object-Oriented Design/Q7_01_Deck_of_Cards/Question.java
+++ Java/Ch 07. Object-Oriented Design/Q7_01_Deck_of_Cards/Question.java
@@ -4,43 +4,44 @@ import java.util.ArrayList;
 
 public class Question {
 
-	
 	public static void main(String[] args) {	
 		int numHands = 5;
 		
 		BlackJackGameAutomator automator = new BlackJackGameAutomator(numHands);
 		automator.initializeDeck();
 		boolean success = automator.dealInitial();
+		if (!success) {
+			System.err.println("Error. Out of cards.");
+			System.exit(1);
+		}
+
+		System.out.println("-- Initial --");
+		automator.printHandsAndScore();
+		ArrayList<Integer> blackjacks = automator.getBlackJacks();
+		if (blackjacks.size() > 0) {
+			System.out.print("Blackjack at ");
+			for (int i : blackjacks) {
+				System.out.print(i + ", ");
+			}
+			System.out.println("");
+		} else {
+			success = automator.playAllHands();
 			if (!success) {
-				System.out.println("Error. Out of cards.");
-			} else {
-			System.out.println("-- Initial --");
+				System.err.println("Error. Out of cards.");
+				System.exit(1);
+			}
+
+			System.out.println("\n-- Completed Game --");
 			automator.printHandsAndScore();
-			ArrayList<Integer> blackjacks = automator.getBlackJacks();
-			if (blackjacks.size() > 0) {
-				System.out.print("Blackjack at ");
-				for (int i : blackjacks) {
+			ArrayList<Integer> winners = automator.getWinners();
+			if (winners.size() > 0) {
+				System.out.print("Winners: ");
+				for (int i : winners) {
 					System.out.print(i + ", ");
 				}
 				System.out.println("");
 			} else {
-				success = automator.playAllHands();
-				if (!success) {
-					System.out.println("Error. Out of cards.");
-				} else {
-					System.out.println("\n-- Completed Game --");
-					automator.printHandsAndScore();
-					ArrayList<Integer> winners = automator.getWinners();
-					if (winners.size() > 0) {
-						System.out.print("Winners: ");
-						for (int i : winners) {
-							System.out.print(i + ", ");
-						}
-						System.out.println("");
-					} else {
-						System.out.println("Draw. All players have busted.");
-					}
-				}
+				System.out.println("Draw. All players have busted.");
 			}
 		}
 	}
