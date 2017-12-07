5562ff55f11c57d21120366710322a5ff7c338ba
Clay Ver Valen <Clay Ver Valen@DESKTOP-8FCGMCI>
2017-12-07 15:18:14 -0800

    Changed to properly implement Fisher-Yates.
    The p-code from Art of Computer Programing, Vol. 2 is:
    Algorithm P (Shuffling). Let (X1, X2, . . . , Xt) be a sequence of t
    numbers to be shuffled.
    P1. [Initialize.] Set j ← t.
    P2. [Generate U.] Generate a random number U, uniformly distributed
    between zero and one.
    P3. [Exchange.] Set k ← ⌊jU⌋ + 1. (Now k is a random integer, between 1
    and j. Exercise 3.4.1–3 explains that k should not be computed by taking
    a remainder modulo j.) Exchange Xk ↔ Xj.
    P4. [Decrease j.] Decrease j by 1. If j > 1, return to step P2. 
    
    Note that once a card has been iterated over and been swapped (even with
    itself), it does not have the opportunity to swap again and the code in
    6th edition allows for this (i.e. when is i increasing and i > 0,
    card[0] still has the potential to be swapped).
    
    Existing iterative version also always performed an extra iteration.
    When i = 0, k always equaled 0 resulting in the card swapping with
    itself.

--- Java/Ch 17. Hard/Q17_02_Shuffle/Question.java
+++ Java/Ch 17. Hard/Q17_02_Shuffle/Question.java
@@ -9,26 +9,24 @@ public class Question {
 		return lower + (int)(Math.random() * (higher - lower + 1));
 	}	
 	
-	public static int[] shuffleArrayRecursively(int[] cards, int i) {
-		if (i == 0) {
-			return cards;
+	public static void shuffleArrayRecursively(int[] cards, int i) {
+		if (i == 1) {
+			return;
 		}
 		
-		/* shuffle elements 0 through index - 1 */
-		shuffleArrayRecursively(cards, i - 1);
-		int k = rand(0, i);		
+		/* shuffle element at index (i - 1) with a random element 0 through i - 1 */
+		int k = rand(0, --i);		
 		
 		/* Swap element k and index */
 		int temp = cards[k];
 		cards[k] = cards[i];
 		cards[i] = temp;
 		
-		/* Return shuffled array */
-		return cards;
- 	}
-	
+		shuffleArrayRecursively(cards, i);
+	}
+
 	public static void shuffleArrayIteratively(int[] cards) { 
-		for (int i = 0; i < cards.length; i++) { 
+		for (int i = cards.length - 1 ; i > 0; i--) {
 			int k = rand(0, i);
 			int temp = cards[k];
 			cards[k] = cards[i];
@@ -39,8 +37,9 @@ public class Question {
 	public static void main(String[] args) {
 		int[] cards = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 		System.out.println(AssortedMethods.arrayToString(cards));
+		shuffleArrayRecursively(cards, cards.length);
+		System.out.println(AssortedMethods.arrayToString(cards));
 		shuffleArrayIteratively(cards);
 		System.out.println(AssortedMethods.arrayToString(cards));
 	}
-
 }
