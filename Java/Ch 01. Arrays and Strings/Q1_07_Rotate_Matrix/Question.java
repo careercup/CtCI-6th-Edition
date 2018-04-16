b0786b8c5d50e3a2b8ca1d1118e812606fbd9816
Nitin <emailstonl@gmail.com>
2018-04-16 00:04:32 -0500

    Removing extra variables not required
    Adding anticlock rotation

--- Java/Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/Question.java
+++ Java/Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/Question.java
@@ -8,33 +8,59 @@ public class Question {
 		if (matrix.length == 0 || matrix.length != matrix[0].length) return false; // Not a square
 		int n = matrix.length;
 		
-		for (int layer = 0; layer < n / 2; layer++) {
-			int first = layer;
-			int last = n - 1 - layer;
-			for(int i = first; i < last; i++) {
-				int offset = i - first;
-				int top = matrix[first][i]; // save top
+		for (int layer = 1; layer <= n / 2; layer++) {
+			for(int i = layer; i <= n-layer; i++) {
+				int top = matrix[i-1][layer-1]; // save top
 
 				// left -> top
-				matrix[first][i] = matrix[last-offset][first]; 			
+				matrix[i-1][layer-1] = matrix[n-layer][i-1]; 			
 
 				// bottom -> left
-				matrix[last-offset][first] = matrix[last][last - offset]; 
+				matrix[n-layer][i-1] = matrix[n-i][n-layer]; 
 
 				// right -> bottom
-				matrix[last][last - offset] = matrix[i][last]; 
+				matrix[n-i][n-layer] = matrix[layer-1][n-i]; 
 
 				// top -> right
-				matrix[i][last] = top; // right <- saved top
+				matrix[layer-1][n-i] = top; // right <- saved top
 			}
 		}
 		return true;
 	}
 	
+	public static boolean rotateAnticlock(int[][] matrix) {
+		if (matrix.length == 0 || matrix.length != matrix[0].length) return false; // Not a square
+		int n = matrix.length;
+		
+		for (int layer = 1; layer <= n / 2; layer++) {
+			for(int i = layer; i <= n-layer; i++) {
+				int top = matrix[i-1][layer-1]; // save top
+
+				// right -> top
+				matrix[i-1][layer-1] = matrix[layer-1][n-i]; 			
+
+				// bottom -> right
+				matrix[layer-1][n-i] = matrix[n-i][n-layer]; 
+
+				// left -> bottom
+				matrix[n-i][n-layer] = matrix[n-layer][i-1]; 
+
+				// top -> bottom
+				matrix[n-layer][i-1] = top; // right <- saved top
+			}
+		}
+		return true;
+	}
+
+	
 	public static void main(String[] args) {
 		int[][] matrix = AssortedMethods.randomMatrix(3, 3, 0, 9);
 		AssortedMethods.printMatrix(matrix);
 		rotate(matrix);
+		System.out.println();
+		AssortedMethods.printMatrix(matrix);
+		
+		rotateAnticlock(matrix);
 		System.out.println();
 		AssortedMethods.printMatrix(matrix);
 	}
