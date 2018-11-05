d3fc0719a69d33173de21ca56a1eec1f083e72ca
Gayle McDowell <g@gayle.com>
2018-11-05 00:29:04 -0800

    10.3: Fixing mistaken variable name

--- Java/Ch 10. Sorting and Searching/Q10_03_Search_in_Rotated_Array/Question.java
+++ Java/Ch 10. Sorting and Searching/Q10_03_Search_in_Rotated_Array/Question.java
@@ -26,7 +26,7 @@ public class Question {
 			} else {
 				return search(a, mid + 1, right, x);
 			}
-		} else if (a[mid] < a[left]) { // Right is normally ordered.
+		} else if (a[mid] < a[right]) { // Right is normally ordered.
 			if (x > a[mid] && x <= a[right]) {
 				return search(a, mid + 1, right, x);
 			} else {
