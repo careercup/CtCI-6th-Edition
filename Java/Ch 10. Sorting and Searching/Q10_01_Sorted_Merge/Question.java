01db17932b433f9b7d697cd6e3a159d102b72028
Gayle McDowell <g@gayle.com>
2018-11-05 00:25:13 -0800

    10.1: Making variable names more clear

--- Java/Ch 10. Sorting and Searching/Q10_01_Sorted_Merge/Question.java
+++ Java/Ch 10. Sorting and Searching/Q10_01_Sorted_Merge/Question.java
@@ -8,17 +8,17 @@ public class Question {
 	/** Merges array
 	 * @param a first array
 	 * @param b second array
-	 * @param lastA number of "real" elements in a
-	 * @param lastB number of "real" elements in b
+	 * @param countA number of "real" elements in a
+	 * @param countB number of "real" elements in b
 	 */
-	public static void merge(int[] a, int[] b, int lastA, int lastB) {
-		int indexMerged = lastB + lastA - 1; /* Index of last location of merged array */
-		int indexA = lastA - 1; /* Index of last element in array b */
-		int indexB = lastB - 1; /* Index of last element in array a */
+	public static void merge(int[] a, int[] b, int countA, int countB) {
+		int indexMerged = countB + countA - 1; /* Index of last location of merged array */
+		int indexA = countA - 1; /* Index of last element in array b */
+		int indexB = countB - 1; /* Index of last element in array a */
 	
 		/* Merge a and b, starting from the last element in each */
 		while (indexB >= 0) {
-			if (indexA >= 0 && a[indexA] > b[indexB]) { /* end of a is bigger than end of b */
+			if (indexA >= 0 && a[indexA] > b[indexB]) { /* end of A is bigger than end of B */
 				a[indexMerged] = a[indexA]; // copy element
 				indexA--; 
 			} else {
