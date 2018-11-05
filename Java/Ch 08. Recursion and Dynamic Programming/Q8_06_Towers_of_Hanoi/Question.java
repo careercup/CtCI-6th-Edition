bfcf0e8eb1ed727288b10ec1e4e3de9d96778aa5
Gayle McDowell <g@gayle.com>
2018-11-04 21:00:28 -0800

    8.6: Cleaning up towers of hanoi code

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_06_Towers_of_Hanoi/Question.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_06_Towers_of_Hanoi/Question.java
@@ -2,19 +2,22 @@ package Q8_06_Towers_of_Hanoi;
 
 public class Question {
 	public static void main(String[] args) {
-		// Set up code.
-		int n = 5;
-		Tower[] towers = new Tower[3];
-		for (int i = 0; i < 3; i++) {
-			towers[i] = new Tower(i);
-		}
-		for (int i = n - 1; i >= 0; i--) {
-			towers[0].add(i);
+		Tower source = new Tower();
+		Tower destination = new Tower();
+		Tower buffer = new Tower();
+		
+		source.name = "s";
+		destination.name = "d";
+		buffer.name = "b";
+		
+		/* Load up tower */
+		int numberOfDisks = 5;
+		for (int disk = numberOfDisks - 1; disk >= 0; disk--) {
+			source.add(disk);
 		}
 		
-		// Copy and paste output into a .XML file and open it with internet explorer.
-		//towers[0].print();
-		towers[0].moveDisks(n, towers[2], towers[1]);
-		//towers[2].print();
+		source.print();
+		source.moveDisks(numberOfDisks, destination, buffer);
+		destination.print();
 	}
 }
