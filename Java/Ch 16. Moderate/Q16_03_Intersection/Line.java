5b95e68cb4ea0a66b713314c692817d703182db8
Gayle McDowell <g@gayle.com>
2018-11-07 11:03:04 -0800

    16.3: Fixing bug with parallel lines

--- Java/Ch 16. Moderate/Q16_03_Intersection/Line.java
+++ Java/Ch 16. Moderate/Q16_03_Intersection/Line.java
@@ -3,15 +3,42 @@ package Q16_03_Intersection;
 public class Line {
 	public double slope;
 	public double yintercept;
+	public Point start;
+	public Point end;
 	
 	public Line(Point start, Point end) {
-		double deltaY = end.y - start.y;
-		double deltaX = end.x - start.x;
-		slope = deltaY / deltaX; // Will be Infinity (not exception) when deltaX = 0 
-		yintercept = end.y - slope * end.x;
+		this.start = start;
+		this.end = end;
+		if (start.x == end.x) {
+			slope = Double.POSITIVE_INFINITY;
+		} else {
+			double deltaY = end.y - start.y;
+			double deltaX = end.x - start.x;
+			slope = deltaY / deltaX; 
+			yintercept = end.y - slope * end.x;
+		}
 	}
-		
-	public void print() {
-		System.out.print("y = " + slope + "x + " + yintercept);
+	
+	public boolean isVertical() {
+		return slope == Double.POSITIVE_INFINITY || slope == Double.NEGATIVE_INFINITY;
+	}
+	
+	@Override
+	public String toString() {
+		return "Line [slope=" + slope + ", yintercept=" + yintercept + ", start=" + start + ", end=" + end + "]";
+	}
+
+	public double getYFromX(double x) {
+		if (isVertical()) {
+			return Double.POSITIVE_INFINITY;
+		}
+		return slope * x + yintercept;
+	}
+	
+	public double getXFromY(double y) {
+		if (isVertical()) {
+			return start.x;
+		}
+		return (y - yintercept) / slope;
 	}
 }
--- Java/Ch 16. Moderate/Q16_03_Intersection/Point.java
+++ Java/Ch 16. Moderate/Q16_03_Intersection/Point.java
@@ -12,4 +12,9 @@ public class Point {
 		this.x = x;
 		this.y = y;
 	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	} 
 }
