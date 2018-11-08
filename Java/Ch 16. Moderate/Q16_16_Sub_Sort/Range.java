12ac72d5b665b3aaf4588f5af70b4b88b5596e68
Gayle McDowell <g@gayle.com>
2018-11-08 13:29:31 -0800

    16.16: Adding new solution, separating out testing code, and changing original solution to return range

---
+++ Java/Ch 16. Moderate/Q16_16_Sub_Sort/Range.java
@@ -0,0 +1,40 @@
+package Q16_16_Sub_Sort;
+
+public class Range {
+	public int start;
+	public int end;
+	public Range(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+	
+	@Override
+	public String toString() {
+		return "Range [start=" + start + ", end=" + end + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + end;
+		result = prime * result + start;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Range other = (Range) obj;
+		if (end != other.end)
+			return false;
+		if (start != other.start)
+			return false;
+		return true;
+	}
+}
