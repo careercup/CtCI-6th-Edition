732242a7845083a692c098d71dfce2a735c2497b
Gayle McDowell <g@gayle.com>
2018-11-07 19:16:36 -0800

    VI.11: Simplifying runtime calculation by avoiding short circuiting

--- Java/Big O/QVI_11_Print_Sorted_Strings/Question.java
+++ Java/Big O/QVI_11_Print_Sorted_Strings/Question.java
@@ -23,14 +23,15 @@ public class Question {
 	}
 	
 	public static boolean isInOrder(String s) {
+		boolean isInOrder = true;
 		for (int i = 1; i < s.length(); i++) {
 			int prev = ithLetter(s.charAt(i - 1));
 			int curr = ithLetter(s.charAt(i));
 			if (prev > curr) {
-				return false;
+				isInOrder = false;
 			}
 		}
-		return true;
+		return isInOrder;
 	}
 	
 	public static char ithLetter(int i) {
