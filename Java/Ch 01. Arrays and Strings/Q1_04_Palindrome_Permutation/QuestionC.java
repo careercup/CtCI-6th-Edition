d432bf521b087c69b7f4f0722915ed01d9ff1d65
sandeep549 <sandeepkumar.dev@gmail.com>
2019-04-21 13:25:49 +0530

    Make it concise

--- Java/Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/QuestionC.java
+++ Java/Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/QuestionC.java
@@ -1,41 +1,20 @@
 package Q1_04_Palindrome_Permutation;
 
 public class QuestionC {
-	/* Toggle the ith bit in the integer. */
-	public static int toggle(int bitVector, int index) {
-		if (index < 0) return bitVector;
-		
-		int mask = 1 << index;
-		if ((bitVector & mask) == 0) {
-			bitVector |= mask;
-		} else {
-			bitVector &= ~mask;
-		}
-		return bitVector;
-	}
-	
-	/* Create bit vector for string. For each letter with value i,
-	 * toggle the ith bit. */
-	public static int createBitVector(String phrase) {
+
+	public static boolean isPermutationOfPalindrome(String phrase) {
+		/* Create bit vector for string. For each letter with value i, toggle the ith bit. */
 		int bitVector = 0;
 		for (char c : phrase.toCharArray()) {
-			int x = Common.getCharNumber(c);
-			bitVector = toggle(bitVector, x);
+			int index = Common.getCharNumber(c);
+			bitVector = (index < 0) ? bitVector : (bitVector ^ (1 << index));
 		}
-		return bitVector;
-	}
-	
-	/* Check that at most one bit is set by subtracting one from the 
-	 * integer and ANDing it with the original integer. */
-	public static boolean checkAtMostOneBitSet(int bitVector) {
+
+		/* Check that at most one bit is set by subtracting one from the
+		 * integer and ANDing it with the original integer. */
 		return (bitVector & (bitVector - 1)) == 0;
 	}
-	
-	public static boolean isPermutationOfPalindrome(String phrase) {
-		int bitVector = createBitVector(phrase);
-		return checkAtMostOneBitSet(bitVector);
-	}
-	
+
 	public static void main(String[] args) {
 		String pali = "Rats live on no evil star";
 		System.out.println(isPermutationOfPalindrome(pali));
