67ba447be5b88c506342479c406607d44118a802
Sorokin <aasorokin@naumen.ru>
2020-07-23 11:40:54 +0500

    Q1 02 Check Permutation: Added check for identical strings

--- Java/Ch 01. Arrays and Strings/Q1_02_Check_Permutation/QuestionB.java
+++ Java/Ch 01. Arrays and Strings/Q1_02_Check_Permutation/QuestionB.java
@@ -1,24 +1,26 @@
 package Q1_02_Check_Permutation;
 
-public class QuestionB {	
+public class QuestionB {
 	public static boolean permutation(String s, String t) {
-		if (s.length() != t.length()) return false; // Permutations must be same length
-		
+        if (s.equals(t) || s.length() != t.length()) { // Permutations must have the same characters, but in different orders and must be same length
+            return false;
+        }
+
 		int[] letters = new int[128]; // Assumption: ASCII
 		for (int i = 0; i < s.length(); i++) {
 			letters[s.charAt(i)]++;
 		}
-		  
+
 		for (int i = 0; i < t.length(); i++) {
 			letters[t.charAt(i)]--;
 		    if (letters[t.charAt(i)] < 0) {
 		    	return false;
 		    }
 		}
-		  
+
 		return true; // letters array has no negative values, and therefore no positive values either
 	}
-	
+
 	public static void main(String[] args) {
 		String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
 		for (String[] pair : pairs) {
